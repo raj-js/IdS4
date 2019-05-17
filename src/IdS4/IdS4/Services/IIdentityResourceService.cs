3c63a681b4f1bcf95dd91a285f52a0591e8cf768
xueguang.zhan <xueguang.zhan@macrowing.com>
2019-05-17 15:49:38 +0800

    IdS4 认证服务 init

---
+++ src/IdS4/IdS4/Services/IIdentityResourceService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdS4.Services
+{
+    public interface IIdentityResourceService
+    {
+        void AddResource();
+
+        void RemoveResource();
+
+        void UpdateResource();
+
+
+    }
+}
